ec162bc523b3b2bf392cba019bfad947d551cded
Tobias Brox <tobias@redpill-linpro.com>
2025-11-14 23:06:38 +0100

    work in progress

---
+++ README.md
@@ -0,0 +1,17 @@
+# icalendar-searcher
+
+This library should contain logic for searching, filtering and sorting icalendar data, as well as logic for storing and representing an icalendar search query.
+
+## Status as of v0.x.x
+
+This library is full of stubbed implementations as for now.  The maintainer will urgently prioritize the bare minimum needed for refactoring the search functionality in the CalDAV library.  Version 1.0 may still contain some stubs, but should contain a relatively well-defined feature set.
+
+## History
+
+This is a spin-off from the python caldav project, started by Tobias Brox in 2025-11, the maintainer of the Python CalDAV client library at that time, in collaboration with the main contributors of the icalendar library.
+
+## License
+
+As for now I'm releasing this under the GNU Affero General Public License v.3.0.  If you find this too restrictive or if this causes license compatibility issues for you, please get in touch with me and I will consider to fix some dual licensing, like it's done with the python CalDAV library.
+
+Any contributor has to accept that the code is released under AGPL v3.0 and at some point in the future may be dual-licensed under some more permissive license, like the EUPL v1.1.
---
+++ src/icalendar_searcher/__init__.py
@@ -0,0 +1,164 @@
+from dataclasses import dataclass
+from dataclasses import field
+from datetime import datetime
+from typing import List
+
+from icalendar.prop import TypesFactory
+
+@dataclass
+class Searcher:
+    """This class will:
+
+    * Allow to build up a calendar search query.
+    * Help filterering or sorting a list of calendar components
+
+    This class is a bit stubbed as of 2025-11, many things not working
+    yet.  This class was split out from the CalDAV library and is
+    intended for generic search logic not related to the CalDAV
+    protocol.  As of 2025-11, the first priority is to make the bare
+    minimum of support needed for supporting refactorings of the
+    CalDAV library.
+
+    Long-term plan is to also allow for adding subqueries that can be
+    bound together with logical AND or OR.  (Will AND every be needed?
+    After all, add different criterias to one Searcher object, and all
+    of them has to match)
+
+    Properties (like SUMMARY, CATEGORIES, etc) are not meant to be
+    sent through the constructor, use the ``add_property_filter``
+    method.  Same goes with sort keys, they can be added through the
+    ``add_sort_key`` method.
+
+    The ``todo``, ``event`` and ``journal`` parameters are booleans
+    for filtering the component type.  If i.e. both todo and
+    journal is set to True, everything but events should be returned.
+    If none is given (the default), all objects should be returned.
+
+    If ``todo`` is given ``include_completed`` defaults to False,
+    which means completed tasks will be tfiltered out.
+
+    ``start`` and ``end`` is giving a time range.  The CalDAV logic
+    will be honored, see RFC4791, section 9.9 for very clear
+    definitions of what should be returned and what should be filtered
+    away.
+
+    ``alarm_start`` and ``alarm_end`` is similar for alarm searching
+
+    If ``expand`` is set to True, recurring objects will be expanded
+    into reccurence objects for the time period specified by ``start``
+    and ``end``.
+
+    Unless you now what you are doing, ``expand`` should probably be
+    set to true - but since ``expand`` can only be combined with a
+    start and end, it's by default set to false.
+
+    The filtering and sorting methods should accept both wrapper
+    objects (for the CalDAV library, those are called
+    CalendarObjectResource and contains extra information like the URL
+    and the client object) and ``icalendar.Calendar`` objects from the
+    icalendar library.  Wrapper objects should have the
+    ``icalendar.Calendar`` object available through a property
+    ``icalendar_instance``.  Even for methods expecting a simple
+    component, a ``Calendar`` should be given.  This is of
+    imporantance for recurrences with exceptions (like, "daily meeting
+    is held at 10:00, except today the meeting is postponed until
+    12:00" may be represented through a calendar with two
+    subcomponents)
+
+    Methods expecting multiple components will always expect a list of
+    calendars (CalDAV-style) - but the algorithms should also be
+    robust enough to handle multiple independent components embedded
+    in a single Calendar.
+    """
+    todo: bool = None
+    event: bool = None
+    journal: bool = None
+    start: datetime = None
+    end: datetime = None
+    alarm_start: datetime = None
+    alarm_end: datetime = None
+    comp_class: Union["CalendarObjectResource", "Component"] = None
+    include_completed: bool = None
+
+    expand: bool = False
+
+    _sort_keys: list = field(default_factory=list)
+    _property_filters: dict = field(default_factory=dict)
+    _property_operator: dict = field(default_factory=dict)
+
+    def add_property_filter(self, key: str, value: Any, operator: str = "contains") -> None:
+        """Adds a filter for some specific iCalendar property.
+
+        Examples of valid iCalendar properties: SUMMARY,
+        LOCATION, DESCRIPTION, DTSTART, STATUS, CLASS, etc
+
+        :param key: must be an icalendar property, i.e. SUMMARY
+        :param value: should adhere to the type defined in the RFC
+        :param operator: Comparision operator ("contains", "==", etc)
+
+        For the operator, the following is (planned to be) supported:
+        
+        * contains - will do a substring match (A search for "summary"
+          "contains" "rain" will return both events with summary
+          "Training session" and "Singing in the rain")
+
+        * == - exact match is required
+
+        * ~ - regexp match
+
+        * <, >, <=, >= - comparision
+
+        * <> or != - inqueality, both supported
+
+        * def, undef - will match if the property is (not) defined.  value can be set to None, will be ignored.
+        """
+        if not operator in ("contains", "undef", "=="):
+            raise NotImplementedError(f"The operator {operator} is not supported yet.")
+        if operator != "undef":
+            self._property_filters[key] = TypesFactory.for_property(key)(value)
+        self._property_operator[key] = operator
+
+    def add_sort_key(self, key: str, reversed: bool = None) -> None:
+        """
+        Special keys "isnt_overdue" and "hasnt_started" is
+        supported, those will compare the DUE (for a task) or the
+        DTSTART with the current wall clock and return a bool.
+
+        Except for that, the sort key should be an icalendar property.
+        """
+        assert key in TypesFactory.types_map or key in ("isnt_overdue", "hasnt_started")
+        self._sort_keys.append((key, reversed))
+
+    def check_component(self, component: Union["Calendar", "CalendarObjectResource"]) -> Union["Calendar", "CalendarObjectResource"]:
+        """
+        Checks if a component (or a recurrence set) matches the filters.
+        Expands a recurring component if needed.  Returns the component.
+        """
+        raise NotImplementedError()
+    
+    def filter(self, components: List[Union["Calendar", "CalendarObjectResource"]]) -> List[Union["Calendar", "CalendarObjectResource"]]:
+        """
+        Filters the components given according to the search
+        criterias, and possibly expanding recurrences.
+
+        This method will not modify the components parameter.  The return
+        value is a new list, and if needed it will be containing new objects.
+        """
+        raise NotImplementedError()
+
+    def sort(self, components: List[Union["Calendar", "CalendarObjectResource"]]) -> List[Union["Calendar", "CalendarObjectResource"]]:
+        """
+        Sorts the components given according to the sort
+        keys.
+
+        This method will not modify the components parameter.  The return
+        value is a new list, and if needed it will be containing new objects.
+        """
+        raise NotImplementedError()
+
+    def sort_key(self, component: Union["Calendar", "CalendarObjectResource"]):
+        """
+        Returns a sortable value from the component, based on the sort keys
+        """
+        raise NotImplementedError()
+        
