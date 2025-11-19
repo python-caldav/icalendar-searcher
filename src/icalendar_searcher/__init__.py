607084584b6b9f27b32be41c2bec586b585f843c
Tobias Brox <tobias@redpill-linpro.com>
2025-11-19 19:24:06 +0100

    does this magically get the python 3.10 style coding to work?

--- src/icalendar_searcher/__init__.py
+++ src/icalendar_searcher/__init__.py
@@ -3,6 +3,8 @@
 This package provides the Searcher class for filtering, sorting, and
 expanding calendar components (VEVENT, VTODO, VJOURNAL).
 """
+## for python 3.9 support
+from __future__ import annotations
 
 from .searcher import Searcher
 
